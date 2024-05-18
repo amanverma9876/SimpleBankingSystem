c3451b16daf4fc97bd5974a6d0f5a7a05c575d77
amanverma9876 <148684872+amanverma9876@users.noreply.github.com>
2024-05-18 12:59:16 +0530

    Update page.js

--- client/app/customers/[account]/page.js
+++ client/app/customers/[account]/page.js
@@ -1,5 +1,5 @@
 "use client";
-import graphQLFetch from "@/app/api/graphQLFetch";
+import graphQLFetch from "../../api/graphQLFetch";
 import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 export default function Customer({ params }) {
